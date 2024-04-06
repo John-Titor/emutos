15bd113a6134f4f23c56a4f6c09e197740c747c3
none <user@example.com>
2024-04-05 22:49:16 -0700

    First cut at 16c950 UART support for IP940, with startup rsconf disabled.

--- bios/ip940.h
+++ bios/ip940.h
@@ -20,14 +20,29 @@
 #define QUART_DLL       (0x00<<2)
 #define QUART_DLM       (0x01<<2)
 #define QUART_FCR       (0x02<<2)
+#define QUART_FCR_FIFOEN    (1<<0)
 #define QUART_EFR       (0x02<<2)
+#define QUART_EFR_CTS       (1<<7)
+#define QUART_EFR_RTS       (1<<6)
+#define QUART_EFR_ENHMODE   (1<<4)
 #define QUART_LCR       (0x03<<2)
+#define QUART_LCR_EXTEN     0xbf
+#define QUART_LCR_DLAB      (1<<7)
+#define QUART_MCR       (0x04<<2)
+#define QUART_MCR_INTEN     (1<<4)
+#define QUART_MCR_PRESCALE  (1<<7)
 #define QUART_LSR       (0x05<<2)
-#define QUART_LSR_TXRDY     (1<<5)
+#define QUART_LSR_THRE      (1<<5)
+#define QUART_LSR_BREAK     (1<<4)
 #define QUART_LSR_RXRDY     (1<<0)
 #define QUART_ICR       (0x05<<2)
 #define QUART_SPR       (0x07<<2)
 
+#define QUART_INDEXED   0x80
+#define QUART_ACR       0x80
+#define QUART_ACR_RDEN      (1<<6)
+#define QUART_CPR       0x81
+#define QUART_TCR       0x82
 
 #endif /* MACHINE_IP940 */
 #endif /* IP940.H */
